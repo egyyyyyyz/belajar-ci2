6d14a3386410c9caac2c5314e58b5e41e68d2316
egyyyyyyz <eggyanza@gmail.com>
2026-04-27 21:01:05 +0700

    sesion

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,6 +5,12 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
-$routes->get('/produk', 'ProdukController::index');
-$routes->get('/keranjang', 'TransaksiController::index');
+
+$routes->get('/', 'Home::index', ['filter' => 'auth']);
+
+$routes->get('login', 'AuthController::login');
+$routes->post('login', 'AuthController::login');
+$routes->get('logout', 'AuthController::logout');
+
+$routes->get('produk', 'Home::produk', ['filter' => 'auth']);
+$routes->get('keranjang', 'Home::keranjang', ['filter' => 'auth']);

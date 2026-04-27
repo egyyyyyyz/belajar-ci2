ca6e854213b43e88d947aaad2675592aef381414
egyyyyyyz <eggyanza@gmail.com>
2026-04-27 22:32:37 +0700

    filter

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,11 +6,11 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 
-$routes->get('/', 'Home::index', ['filter' => 'auth']);
+$routes->get('/', 'Home::index', ['filter' => 'auth']); //mengakses rute ini harus login dulu
 
 $routes->get('login', 'AuthController::login');
 $routes->post('login', 'AuthController::login');
 $routes->get('logout', 'AuthController::logout');
 
-$routes->get('produk', 'Home::produk', ['filter' => 'auth']);
-$routes->get('keranjang', 'Home::keranjang', ['filter' => 'auth']);
+$routes->get('produk', 'ProdukController::index', ['filter' => 'auth']); //mengakses rute ini harus login
+$routes->get('keranjang', 'TransaksiController::index', ['filter' => 'auth']); //mengakses rute ini harus login
